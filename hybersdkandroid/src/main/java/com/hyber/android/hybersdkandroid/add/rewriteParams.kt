5003fe2ec3b10c6433a0f07886858101cd43d359
Dzmitry Buynovskiy <imperituro.ard@gmail.com>
2020-09-14 22:26:01 +0300

    Cleaning

--- hybersdkandroid/src/main/java/com/hyber/android/hybersdkandroid/add/rewriteParams.kt
+++ hybersdkandroid/src/main/java/com/hyber/android/hybersdkandroid/add/rewriteParams.kt
@@ -9,27 +9,27 @@ internal class RewriteParams(val context: Context) {
     private val sharedPreference: SharedPreference = SharedPreference(context)
 
     fun rewriteHyberUserMsisdn(hyber_user_msisdn: String) {
-        sharedPreference.save("hyber_user_msisdn", hyber_user_msisdn)
+        sharedPreference.saveString("hyber_user_msisdn", hyber_user_msisdn)
     }
 
     fun rewriteHyberUserPassword(hyber_user_password: String) {
-        sharedPreference.save("hyber_user_Password", hyber_user_password)
+        sharedPreference.saveString("hyber_user_Password", hyber_user_password)
     }
 
     fun rewriteHyberRegistrationToken(hyber_registration_token: String) {
-        sharedPreference.save("hyber_registration_token", hyber_registration_token)
+        sharedPreference.saveString("hyber_registration_token", hyber_registration_token)
     }
 
     fun rewriteHyberUserId(hyber_user_id: String) {
-        sharedPreference.save("hyber_user_id", hyber_user_id)
+        sharedPreference.saveString("hyber_user_id", hyber_user_id)
     }
 
     fun rewriteHyberDeviceId(deviceId: String) {
-        sharedPreference.save("deviceId", deviceId)
+        sharedPreference.saveString("deviceId", deviceId)
     }
 
     fun rewriteHyberCreateAt(hyber_registration_createdAt: String) {
-        sharedPreference.save("hyber_registration_createdAt", hyber_registration_createdAt)
+        sharedPreference.saveString("hyber_registration_createdAt", hyber_registration_createdAt)
     }
 
     fun rewriteApiRegistrationStatus(registrationstatus: Boolean) {
@@ -37,7 +37,7 @@ internal class RewriteParams(val context: Context) {
     }
 
     fun rewriteFirebaseToken(fb_token_new: String) {
-        sharedPreference.save("firebase_registration_token", fb_token_new)
+        sharedPreference.saveString("firebase_registration_token", fb_token_new)
     }
 
 }
